f779764935d927debc16e3a57b787c06d1eae91f
paulconway <paulconway0@gmail.com>
2014-12-08 10:32:28 +0000

    Tweak

--- Assets/Class 7 - Various Bits/Perception.cs
+++ Assets/Class 7 - Various Bits/Perception.cs
@@ -8,7 +8,13 @@ public class Perception : MonoBehaviour {
 
 	void Update () 
 	{
-		Vector3 forwardDirection = transform.TransformDirection(Vector3.right); //give our player a default facing direction, updates with it's rotation
+		FieldOfView();
+		//InFrontOrBehind();
+	}
+	
+	void FieldOfView() //if you want to spot a target within a specific angle, or cone of vision
+	{
+		Vector3 forwardDirection = transform.TransformDirection(Vector3.right); //give our player a default facing direction, change this is you like! It updates with it's rotation
 		Vector3 vectorToOther = target.position - transform.position; 
 		vectorToOther.Normalize(); //makes the vector of length 1 (x, y, z componenet of vector each divided by the length or magnitude of the vector
 		//magnitude is caculated as the square root of x2, y2, z2, so for example (4, 3, 0) = 16 + 9 + 0 = 25. Square root of 25 is 5, which is the length or magnitude of the vector
@@ -32,5 +38,29 @@ public class Perception : MonoBehaviour {
 			target.renderer.material.color = Color.white;
 		}
 		Debug.Log (angle);
+	}
+	
+	void InFrontOrBehind()
+	{
+		Vector3 forwardDirection = transform.TransformDirection(Vector3.right); //give our player a default facing direction, change this is you like! It updates with it's rotation
+		Vector3 vectorToOther = target.position - transform.position; 
+		vectorToOther.Normalize(); //makes the vector of length 1 (x, y, z componenet of vector each divided by the length or magnitude of the vector
+		//magnitude is caculated as the square root of x2, y2, z2, so for example (4, 3, 0) = 16 + 9 + 0 = 25. Square root of 25 is 5, which is the length or magnitude of the vector
+		
+		Debug.DrawLine (transform.position, target.position);
+		Debug.DrawLine (transform.position, transform.position + forwardDirection * 4);
+		
+		//float dotMath = (forwardDirection.x * forwardDirection.x) + (forwardDirection.y * vectorToOther.y) + (forwardDirection.z * vectorToOther.z); //this way works too!
+		float dot = Vector3.Dot(vectorToOther, forwardDirection); //takes two vectors and returns the dot product between them (same as calculation in line above!)
+		
+		if(dot > 0) //if the angle of dot is less than 0, target is in front of the player!
+		{
+			Debug.Log ("Spotted!");
+			target.renderer.material.color = Color.red;
+		}
+		else
+		{
+			target.renderer.material.color = Color.white;
+		}
 	}
 }
